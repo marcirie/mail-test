f9335672f3b724b81264bdb667c2085b7d5521bb
Marc Irié <iriebigalamarc@gmail.com>
2022-01-07 15:29:20 +0100

    continuité

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const tableau = ['marcirie23gmail.com', 'marcirie23@gmailcom', 'marcirie23@gmail.com', 'marcirie23@gmail.com', 'marcirie23@gmail.com']
+/*const tableau = ['marcirie23gmail.com', 'marcirie23@gmailcom', 'marcirie23@gmail.com',
+    'marcirie23@gmail.com', 'marcirie23@gmail.com']*/
+
+console.log(process.argv);
+    const tableau = process.argv[2].split(',')
 
 const validator = require('validator')
+
 for (let item = 0; item < tableau.length; item++) {
     const element = tableau[item];
     
     let isEmail = validator.isEmail(element)
 
-    if ( isEmail = validator.isEmail(element)) {
+    if ( validator.isEmail(element)) {
         console.log(element, ': ok');
     } else {
         console.log(element, ": no");
